67f4584457e33a21f91c7773ccad14ba22499d5b
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-03-09 17:38:48 -0300

    Add tangent plane plotting function (Plot_Function.py file) and gitinore

---
+++ Plot_Function.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def tangent_plane(function, grid_start, grid_end, interval):
+    X = np.arange(grid_start[0] + interval[0], grid_end[0], interval[0])
+    Y = np.arange(grid_start[1] + interval[1], grid_end[1], interval[1])
+    grid = np.meshgrid(X, Y)
+    U = np.ones((len(X), len(Y)))
+    V = np.vectorize(function)(grid[0], grid[1])
+
+    fig, ax = plt.subplots()
+    q = ax.quiver(X, Y, U, V)
+    ax.quiverkey(q, X=0.3, Y=1.1, U=10, label='Quiver key, length = 10', labelpos='E')
+
+    plt.show()
