b593b2e1a43dd19a6d78b69c37e7ba2e75463387
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-04-08 21:53:14 -0300

    Add Convergences.py file with convergence calculation functions

---
+++ Convergences.py
@@ -0,0 +1,31 @@
+import numpy as np
+from Comp_Methods import *
+import math
+
+def total_convergence(f, t0, y0, t, delta_t, N, method, exact):
+    y = exact(t)
+    entries = []
+    for i in range(N):
+        h = delta_t / 2**i
+        n1 = method(f, t0, y0, t, h)[1][-1]
+        n2 = method(f, t0, y0, t, h/2)[1][-1]
+        e1 = abs(n1-y)
+        e2 = abs(n2-y)
+        q = e1/e2
+        p = math.log2(q)
+        entries.append([i, h, e1, q, p])
+    return entries
+
+def estimated_convergence(f, t0, y0, t, delta_t, N, method):
+    entries = []
+    for i in range(N):
+        h = delta_t / 2**i
+        n1 = method(f, t0, y0, t, h)[1][-1]
+        n2 = method(f, t0, y0, t, h/2)[1][-1]
+        n3 = method(f, t0, y0, t, 2*h)[1][-1]
+        e1 = abs(n3-n1)
+        e2 = abs(n1-n2)
+        q = e1/e2
+        p = math.log2(q)
+        entries.append([i, h, e1, q, p])
+    return entries
