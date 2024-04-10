c5b73ae8c1c70d3a62b1d026c3709589328d15ac
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-04-09 22:11:07 -0300

    Readjust figure size

--- Plot_Function.py
+++ Plot_Function.py
@@ -8,6 +8,6 @@ def tangent_plane(function, grid_start, grid_end, interval):
     U = np.ones((len(X), len(Y)))
     V = np.vectorize(function)(grid[0], grid[1])
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 10))
     q = ax.quiver(X, Y, U, V)
     ax.quiverkey(q, X=0.3, Y=1.1, U=10, label='Quiver key, length = 10', labelpos='E')
