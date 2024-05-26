0506e73f37ad7830b78a5221427d5f6d4fb86795
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-05-26 11:53:09 -0300

    feat: add stability region calculation functions to P1Q5 class

--- P1Q5.py
+++ P1Q5.py
@@ -4,10 +4,11 @@ import math
 import random
 
 class P1Q5:
-    def __init__(self, phi):
+    def __init__(self, phi, w):
         self.phi = phi
         self.t0 = 0
         self.y0 = 1
+        self.w = w
     
     def psi(self, l, delta_t, n):
         f = lambda y: l * y
@@ -22,8 +23,40 @@ class P1Q5:
         return np.mean(phi)
     
     def _calculateAbsStabInt(self):
-        delta_t = 0
+        delta_t = 0.01
+        t = 0
         l = -1
-        while self.psi(l, delta_t, 100) <= 1:
-            delta_t += 0.01
+        while self.psi(l, t, 100) <= 1:
+            t += delta_t
         self.absInt = (-delta_t, 0)
+        print("Absolute Stability Interval: ", self.absInt)
+
+    def _StabRegWithMonteCarlo(self, n):
+        delta_t = 1
+        limit = (1/self.w) + 1
+        valids = []
+        for n in range(n):
+            re = random.uniform(-limit, 1)
+            im = random.uniform(-limit, limit)
+            num = complex(re, im)
+            if self.psi(num, delta_t, 100) <= 1:
+                valids.append(num)
+        plot = plt.figure()
+        plt.scatter([x.real for x in valids], [x.imag for x in valids])
+        plt.show()
+
+    def _StabRegWithLevelCurves(self, n, m, amp_func):
+        delta_t = 1
+        limit = (1/self.w) + 1
+        re = np.linspace(-limit, 1, n)
+        im = np.linspace(-limit, limit, n)
+        Re, Im = np.meshgrid(Re, Im)
+        vec_psi = np.vectorize(self.psi)
+        Z = vec_psi(complex(Re, Im), delta_t, 100)
+        plot = plt.figure()
+        plt.contour(Re, Im, Z, levels=np.linspace(-1, 1, m), cmap='viridis')
+        plt.show()
+
+
+
+    
